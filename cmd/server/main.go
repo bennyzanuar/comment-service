e0de51e99a5864cbf315fde63574b3cc424cb4b8
Benny Zanuarwan PG <benny.zanuarwan@tanihub.com>
2022-01-15 16:24:34 +0700

    sturcturing application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointers
+// to database connections
+type App struct{}
+
+// Run - sets up application
+func (app *App) Run() error {
+	fmt.Println("Setting up application")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go rest api")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up application")
+		fmt.Println(err)
+	}
 }
